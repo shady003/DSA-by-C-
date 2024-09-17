0074d14229a5be2917505a940dbfe9c41858743c
Saurabh <saurabhyadav9909@gmail.com>
2024-09-17 22:22:14 +0530

    intersection of two array

--- intersectionArray.cpp
+++ intersectionArray.cpp
@@ -1,35 +1,45 @@
 #include <bits/stdc++.h> 
+#include <iostream>
 #include <vector>
 using namespace std;
-vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
-{	vector<int> ans;
-	int i=0; int j=0;
-	while(i<n&&j<m){
-		if(arr1[i]==arr2[j]){
-			ans.push_back(arr1[i]);
-			i++;
-			j++;
-		}
-		else if(arr2[j]>arr1[i]){
-			i++;
-		}
-		else{
-			j++;
-		}
-	}
-	return ans;
-}
-int main(){
-    vector<int> arr1 = {1,2,3,4,5};
-    vector<int> arr2 = {3,4,5};
-    int n = arr1.size();
-    int m = arr2.size();
-    vector<int> result = findArrayIntersection(arr1, n, arr2, m);
-    cout << "Intersection of two arrays is: ";
-    for (int i = 0; i < result.size(); i++) {   
-        cout << result[i] << " ";
-        }   
-        return 0;
-        
 
+// Function to find the intersection of two sorted arrays
+vector<int> findArrayIntersection(int arr1[], int n, int arr2[], int m)
+{
+    vector<int> ans;
+    int i = 0, j = 0;
+
+    // Loop until one of the arrays is fully traversed
+    while (i < n && j < m) {
+        if (arr1[i] == arr2[j]) {
+            ans.push_back(arr1[i]);  // If elements are equal, add to the result
+            i++;
+            j++;
+        }
+        else if (arr2[j] > arr1[i]) {
+            i++;  // Move pointer `i` when arr1[i] is smaller
+        }
+        else {
+            j++;  // Move pointer `j` when arr2[j] is smaller
+        }
+    }
+    return ans;  // Return the result vector
+}
+
+int main() {
+    int arr1[5] = {1, 2, 3, 4, 5};
+    int arr2[3] = {3, 4, 5};
+    int n = 5;
+    int m = 3;
+
+    // Find the intersection of the two arrays
+    vector<int> result = findArrayIntersection(arr1, n,arr2, m);
+
+    // Output the result
+    cout << "Intersection of two arrays is: ";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";  // Print each element in the result
+    }
+
+    return 0;
 }
